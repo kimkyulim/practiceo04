7849d5cdbe8d2735c01af16f6d4e6c263c79989c
bit-user <bit-user@DESKTOP-OG5SKF3>
2017-08-03 17:56:17 +0900

    commint 0803 practice

--- src/prob02/BookShop.java
+++ src/prob02/BookShop.java
@@ -23,6 +23,7 @@ public class BookShop {
 		int num = scanner.nextInt();
 		scanner.close();
 		
+		
 		// (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
 		for( Book book : books ) {
 			if( num == book.getBookNo() ) {
@@ -35,10 +36,11 @@ public class BookShop {
 		System.out.println("*****도서 정보 출력하기******");
 		displayBookInfo( books );
 	}
-
-	private static void displayBookInfo(Book[] books) {
-		System.out.println();
-		
+	
+	private static void displayBookInfo( Book[] books ) {
+		for( Book book : books ) {
+			book.print();
+		}
 	}
 
 }
